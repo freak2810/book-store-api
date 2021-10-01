0621621d659c519b6f5be6f872a757965421c8c8
freak2810 <adityamanikanthrao@gmail.com>
2021-10-01 16:12:04 +0530

    started building schema

--- resolvers/resolvers.ts
+++ resolvers/resolvers.ts
@@ -3,6 +3,17 @@ import { Resolvers } from '../schema/graphql.generated';
 const resolvers: Resolvers = {
 	Query: {
 		helloWorld: () => 'Hello World!',
+		book: () => [
+			{
+				__typename: 'Book',
+				title: 'Harry Potter',
+				description: 'A book about a wizard',
+				genre: ['Fantasy', 'Adventure'],
+				price: 45.99,
+				rating: 0,
+				yearOfPublication: 2000,
+			},
+		],
 	},
 };
 
